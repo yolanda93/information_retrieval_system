d8ae36133fd7c6435bcc42b927d13471f7f917c0
yolanda_de_la_hoz_simon <yolanda93h@gmail.com>
2016-03-06 16:54:07 +0100

    commit test IDE

--- vsm.py
+++ vsm.py
@@ -6,55 +6,61 @@ from gensim import corpora, models, similarities
 from operator import itemgetter
 
 sample_corpus = [
-									"Human machine interface for lab abc computer applications",
-              		"A survey of user opinion of computer system response time",
-              		"The EPS user interface management system",
-              		"System and human system engineering testing of EPS",
-              		"Relation of user perceived response time to error measurement",
-              		"The generation of random binary unordered trees",
-              		"The intersection graph of paths in trees",
-              		"Graph minors IV Widths of trees and well quasi ordering",
-              		"Graph minors A survey"
-								]
+    "Human machine interface for lab abc computer applications",
+    "A survey of user opinion of computer system response time",
+    "The EPS user interface management system",
+    "System and human system engineering testing of EPS",
+    "Relation of user perceived response time to error measurement",
+    "The generation of random binary unordered trees",
+    "The intersection graph of paths in trees",
+    "Graph minors IV Widths of trees and well quasi ordering",
+    "Graph minors A survey"
+]
+
 
 def preprocess_document(doc):
-	stopset = set(stopwords.words('english'))
-	stemmer = PorterStemmer()
-	tokens = wordpunct_tokenize(doc)
-	clean = [token.lower() for token in tokens if token.lower() not in stopset and len(token) > 2]
-	final = [stemmer.stem(word) for word in clean]
-	return final
+    stopset = set(stopwords.words('english'))
+    stemmer = PorterStemmer()
+    tokens = wordpunct_tokenize(doc)
+    clean = [token.lower() for token in tokens if token.lower() not in stopset and len(token) > 2]
+    final = [stemmer.stem(word) for word in clean]
+    return final
+
 
 def create_dictionary(docs):
-	pdocs = [preprocess_document(doc) for doc in docs]
-	dictionary = corpora.Dictionary(pdocs)
-	dictionary.save('/tmp/vsm.dict')
-	return dictionary
+    pdocs = [preprocess_document(doc) for doc in docs]
+    dictionary = corpora.Dictionary(pdocs)
+    dictionary.save('/tmp/vsm.dict')
+    return dictionary
+
 
 def get_keyword_to_id_mapping(dictionary):
-	print dictionary.token2id
+    print dictionary.token2id
+
 
 def docs2bows(corpus, dictionary):
-	docs = [preprocess_document(d) for d in corpus]
-	vectors = [dictionary.doc2bow(doc) for doc in docs]
-	corpora.MmCorpus.serialize('/tmp/vsm_docs.mm', vectors)
-	return vectors
+    docs = [preprocess_document(d) for d in corpus]
+    vectors = [dictionary.doc2bow(doc) for doc in docs]
+    corpora.MmCorpus.serialize('/tmp/vsm_docs.mm', vectors)
+    return vectors
+
 
 def create_TF_IDF_model(corpus):
-	dictionary = create_dictionary(corpus)
-	docs2bows(corpus, dictionary)
-	loaded_corpus = corpora.MmCorpus('/tmp/vsm_docs.mm')
-	tfidf = models.TfidfModel(loaded_corpus)
-	return tfidf, dictionary
+    dictionary = create_dictionary(corpus)
+    docs2bows(corpus, dictionary)
+    loaded_corpus = corpora.MmCorpus('/tmp/vsm_docs.mm')
+    tfidf = models.TfidfModel(loaded_corpus)
+    return tfidf, dictionary
+
 
 def launch_query(corpus, q):
-	tfidf, dictionary = create_TF_IDF_model(corpus)
-	loaded_corpus = corpora.MmCorpus('/tmp/vsm_docs.mm')
-	index = similarities.MatrixSimilarity(loaded_corpus, num_features=len(dictionary))
-	pq = preprocess_document(q)
-	vq = dictionary.doc2bow(pq)
-	qtfidf = tfidf[vq]
-	sim = index[qtfidf]
-	ranking = sorted(enumerate(sim), key=itemgetter(1), reverse=True)
-	for doc, score in ranking:
-		print "[ Score = " + "%.3f" % round(score,3) + "] " + corpus[doc]; 
+    tfidf, dictionary = create_TF_IDF_model(corpus)
+    loaded_corpus = corpora.MmCorpus('/tmp/vsm_docs.mm')
+    index = similarities.MatrixSimilarity(loaded_corpus, num_features=len(dictionary))
+    pq = preprocess_document(q)
+    vq = dictionary.doc2bow(pq)
+    qtfidf = tfidf[vq]
+    sim = index[qtfidf]
+    ranking = sorted(enumerate(sim), key=itemgetter(1), reverse=True)
+    for doc, score in ranking:
+        print "[ Score = " + "%.3f" % round(score, 3) + "] " + corpus[doc];
