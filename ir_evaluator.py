ae8f966649b6b491558dd33a760204a85b38299d
Yolanda de la Hoz Simón <yolanda93h@gmail.com>
2016-05-24 19:01:29 +0100

    total relevant docs for each query

--- ir_evaluator.py
+++ ir_evaluator.py
@@ -12,11 +12,11 @@ class IREvaluator(object):
         if len(queries) >1: # Evaluate queries
            for q in queries:
                print("\n-------------------------->Query = " + q ) 
-               self.total_relevant_docs(query_id)
+               total_relevant_docs=self.get_total_relevant_docs()
                query_id=query_id+1
         else:
             print("\n-------------------------->Query = " + queries ) 
-            self.total_relevant_docs(self,queries)
+            self.get_total_relevant_docs(self,queries)
 
 
     #################################################################################
@@ -34,13 +34,17 @@ class IREvaluator(object):
     #  @details This method returns the total relevant documents for a query.
     #  @param   query_id The id of the query
     #################################################################################    
-    def total_relevant_docs(self,query_id): 
-        relevant_docs=0     
+    def get_total_relevant_docs(self): 
+        relevant_docs=0   
+        query_id=0 
+        total_relevant_docs=[]
         for doc in self.relevance_docs:
             if(int(doc[0])==query_id): # the position 0 contains the query ID
               relevant_docs=relevant_docs+1
             if(int(doc[0])>query_id): # relevance docs csv are ordered, we stop to iterate
-              return relevant_docs
-        return relevant_docs
+              query_id=query_id+1
+              total_relevant_docs.append(relevant_docs);
+              relevant_docs=0               
+        return total_relevant_docs
 
 
